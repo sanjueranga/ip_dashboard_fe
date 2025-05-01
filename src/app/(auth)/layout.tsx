2264997e71bb51e36a801e88dffc35af5dd1f64f
Lakshitha Eranga <sanjueranga16@gmail.com>
2025-05-01 22:54:17 +0530

    WIP

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+// Inner layout file (e.g., app/dashboard/layout.tsx)
+import { Header } from "@/components/Layouts/header";
+import { Sidebar } from "@/components/Layouts/sidebar";
+import type { PropsWithChildren } from "react";
+
+export default function DashboardLayout({ children }: PropsWithChildren) {
+  return (
+    <div className="flex min-h-screen">
+      {/* <Sidebar /> */}
+      <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
+        <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
